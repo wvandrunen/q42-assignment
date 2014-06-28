9e5385bfff204c3a93d053ccb84f484bb45cdc94
Wouter van Drunen <wvandrunen@gmail.com>
2014-06-28 13:21:56 +0200

    fixed application and added ScalaStyle

--- src/main/scala/com/q42/Application.scala
+++ src/main/scala/com/q42/Application.scala
@@ -4,17 +4,20 @@ object Application {
 
   def main(args: Array[String]): Unit = {
 
-    val solutionGeneratorFactory = SolutionGeneratorFactory
+    val numbers: List[Int] = List(2,4,8,16,32)
+    val operators: List[String] = List("*", "+", "-", "/")
+
+    val solutionGenerator = new SolutionGenerator(numbers, operators)
 
     println()
     println("=======================")
     println("Starting Q42 Assignment")
     println("=======================")
 
-    println("Numbers used    : " + solutionGeneratorFactory.numbers)
-    println("Operators used  : " + solutionGeneratorFactory.operators)
+    println("Numbers used    : " + solutionGenerator)
+    println("Operators used  : " + solutionGenerator)
 
-    val calculator = new SolutionCalculator(new ReversedPolishNotatedFormula, solutionGeneratorFactory.create)
+    val calculator = new SolutionCalculator(new ReversedPolishNotatedFormula, solutionGenerator)
     val solutions = calculator.calculateSolutions()
 
     println("Solutions found : " + solutions.size)
